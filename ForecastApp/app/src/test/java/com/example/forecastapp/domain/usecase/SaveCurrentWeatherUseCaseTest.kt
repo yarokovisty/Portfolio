92f3ff138843257943c9374a6f9ca16c13eb03e4
YarokivistY <65013098+yarokovisty@users.noreply.github.com>
2024-07-10 20:42:33 +0700

    feat: написать unit тест на SaveCurrentWeatherUseCase

---
+++ ForecastApp/app/src/test/java/com/example/forecastapp/domain/usecase/SaveCurrentWeatherUseCaseTest.kt
@@ -0,0 +1,37 @@
+package com.example.forecastapp.domain.usecase
+
+import com.example.forecastapp.domain.entity.CurrentWeatherItem
+import com.example.forecastapp.domain.repository.ForecastRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+
+class SaveCurrentWeatherUseCaseTest {
+
+    private val repository = mock<ForecastRepository>()
+    private lateinit var saveCurrentWeatherUseCase: SaveCurrentWeatherUseCase
+
+    @BeforeEach
+    fun setUp() {
+        saveCurrentWeatherUseCase = SaveCurrentWeatherUseCase(repository)
+    }
+
+    @Test
+    fun `invoke() should call saveCurrentWeather on repository with currentWeatherItem`() = runTest {
+        val currentWeatherItem = CurrentWeatherItem(
+            800,
+            "Sunny",
+            25,
+            3,
+            30,
+            10
+        )
+
+        saveCurrentWeatherUseCase(currentWeatherItem)
+
+        verify(repository, times(1)).saveCurrentWeather(currentWeatherItem)
+    }
+}
