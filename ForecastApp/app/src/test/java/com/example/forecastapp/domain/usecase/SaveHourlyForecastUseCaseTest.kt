2ffd16b99474e91f1787375975a7a68180f34982
YarokivistY <65013098+yarokovisty@users.noreply.github.com>
2024-07-10 20:49:41 +0700

    feat: написать unit тест на SaveHourlyForecastUseCase

---
+++ ForecastApp/app/src/test/java/com/example/forecastapp/domain/usecase/SaveHourlyForecastUseCaseTest.kt
@@ -0,0 +1,49 @@
+package com.example.forecastapp.domain.usecase
+
+import com.example.forecastapp.domain.entity.CurrentWeatherItem
+import com.example.forecastapp.domain.entity.HourlyForecastItem
+import com.example.forecastapp.domain.repository.ForecastRepository
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+
+class SaveHourlyForecastUseCaseTest {
+
+    private val repository = mock<ForecastRepository>()
+    private lateinit var saveHourlyForecastUseCase: SaveHourlyForecastUseCase
+
+    @BeforeEach
+    fun setUp() {
+        saveHourlyForecastUseCase = SaveHourlyForecastUseCase(repository)
+    }
+
+    @Test
+    fun `invoke() should call saveHourlyForecast on repository with hourlyForecastItem`() =
+        runTest {
+            val listHourlyForecastItem = listOf(
+                HourlyForecastItem(
+                    801,
+                    "12:00",
+                    26
+                ),
+                HourlyForecastItem(
+                    803,
+                    "15:00",
+                    23
+                ),
+                HourlyForecastItem(
+                    802,
+                    "18:00",
+                    28
+                )
+            )
+
+            saveHourlyForecastUseCase(listHourlyForecastItem)
+
+            verify(repository, times(1)).saveHourlyForecast(listHourlyForecastItem)
+
+        }
+}
